f2f4ee9605103f619f7384db578b947879f86ad6
bcapuano <bcapuano@asu.edu>
2023-05-13 19:00:24 -0400

    Initial Mayhem Heroes Integration

---
+++ mayhem/main.swift
@@ -0,0 +1,17 @@
+#if canImport(Darwin)
+import Darwin.C
+#elseif canImport(Glibc)
+import Glibc
+#elseif canImport(MSVCRT)
+import MSVCRT
+#endif
+
+import Foundation
+import Expression
+
+@_cdecl("LLVMFuzzerTestOneInput")
+public func test(_ start: UnsafeRawPointer, _ count: Int) -> CInt {
+    let fdp = FuzzedDataProvider(start, count)
+    Expression.parse(fdp.ConsumeRemainingString(), usingCache: false)
+    return 0;
+}
